529265d77a38c8a532fcca6dd383db9195b84c19
arulkg17 <arulkg@yahoo.com>
2026-08-05 19:32:04 -0500

    Implement JWT protected routes and logout functionality

--- src/routes/AppRoutes..tsx
+++ src/routes/AppRoutes..tsx
@@ -1,12 +1,26 @@
 import { Routes, Route } from "react-router-dom";
+
 import Login from "../pages/login/Login";
 import Dashboard from "../pages/dashboard/Dashboard";
+import Logout from "../pages/logout/Logout";
+
+import ProtectedRoute from "./ProtectedRoute";
+
 export default function AppRoutes() {
   return (
     <Routes>
+      {/* Public Routes */}
       <Route path="/" element={<Login />} />
+
       <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+
+      {/* Protected Routes */}
+      <Route element={<ProtectedRoute />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Route>
+
+      {/* Logout */}
+      <Route path="/logout" element={<Logout />} />
     </Routes>
   );
 }
